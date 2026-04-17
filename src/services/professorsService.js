0d6a2a2b8f82cfa40e69f11a6a9d5d224a7eb8ae
AlbaGG95 <albaganduxe95@gmail.com>
2026-04-17 13:13:48 +0200

    feat: integrate professors service with API and adapter

--- src/services/professorsService.js
+++ src/services/professorsService.js
@@ -1,9 +1,22 @@
 import { professors } from '../data/dashboardData'
+import httpClient from './httpClient'
 
-export function getProfessors() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(professors)
-    }, 300)
-  })
+function mapProfessor(professor) {
+  return {
+    id: professor.id,
+    name: professor.name ?? professor.firstName,
+    dni: professor.dni,
+    hiringYear: professor.hiringYear ?? professor.hiring_year,
+    isActive: professor.isActive ?? professor.is_hired ?? true,
+    image: professor.image,
+  }
+}
+
+export async function getProfessors() {
+  try {
+    const response = await httpClient.get('/professors')
+    return response.data.map(mapProfessor)
+  } catch {
+    return professors.map(mapProfessor)
+  }
 }
