b0cf904310ea7466f21c2ffeed1f347862e6f489
AlbaGG95 <albaganduxe95@gmail.com>
2026-04-17 13:06:06 +0200

    feat: add user adapter to normalize API data

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,11 +1,22 @@
 import { users } from '../data/dashboardData'
 import httpClient from './httpClient'
 
+function mapUser(user) {
+  return {
+    id: user.id,
+    name: user.name || user.firstName,
+    surname: user.surname || user.lastName,
+    dni: user.dni,
+    isActive: user.isActive ?? true,
+    image: user.image,
+  }
+}
+
 export async function getUsers() {
   try {
     const response = await httpClient.get('/users')
-    return response.data
+    return response.data.map(mapUser)
   } catch {
-    return users
+    return users.map(mapUser)
   }
 }
