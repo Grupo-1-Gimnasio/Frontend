e32f74c46f269a7dc93f59106f92cf4b70458665
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-04-23 22:13:34 +0200

    last details

--- src/components/home/TestimonialCard.jsx
+++ src/components/home/TestimonialCard.jsx
@@ -2,7 +2,7 @@ function TestimonialCard({ image, alt }) {
   return (
     <article
       role="article"
-      className="mx-auto aspect-[2/3] w-full max-w-[320px] overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-900/90 transition duration-200 hover:-translate-y-1 hover:border-orange-500/40 hover:bg-neutral-900 focus-within:ring-2 focus-within:ring-orange-500/60 focus-within:ring-offset-2 focus-within:ring-offset-[#111111]"
+      className="mx-auto aspect-[2/3] w-full max-w-[320px] overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-900/90 transition duration-200 hover:-translate-y-1 hover:border-orange-500 hover:bg-neutral-900 focus-within:ring-2 focus-within:ring-orange-500/60 focus-within:ring-offset-2 focus-within:ring-offset-[#111111]"
     >
       <img
         src={image}
